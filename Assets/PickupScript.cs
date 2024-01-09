eaadae50fbea6a6f3ad9c6cab7d257cef7a1d99c
ErikHatesHw <114367948+ErikEatsDoritos@users.noreply.github.com>
2024-01-09 13:10:44 -0500

    Created Item collecting

--- Assets/PickupScript.cs
+++ Assets/PickupScript.cs
@@ -6,21 +6,26 @@ public class PickupScript : MonoBehaviour
 {
     // Start is called before the first frame update
     private PlayerInventory inventory;
+    public GameObject itembutton;
     void Start()
     {
         
         inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerInventory>();
     }
 
-    public void OnTriggerEnter(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))){
+        if (other.CompareTag("Player")){
             for (int i = 0; i < inventory.slots.Length; i++)
             {
                 if (inventory.IsFull[i] == false)
                 {
                     //ADD ITEM TO INVENTORY
-                    
+                    Debug.Log("picked up item");
+                    inventory.IsFull[i] = true;
+                    //Instantiate(itembutton, inventory.slots[i].transform);
+                    Destroy(gameObject);
+                    break;
                 }
             }
         }
