9350f0a090d6147c24c460be1d01afc5b0bb3c16
TomatoesAreGood <139704217+TomatoesAreGood@users.noreply.github.com>
2024-01-14 12:40:29 -0500

    Slot Dimming

--- Assets/Scripts/Inventory/Slot.cs
+++ Assets/Scripts/Inventory/Slot.cs
@@ -1,34 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
+
 using UnityEngine.EventSystems;
 
 
 public class Slot : MonoBehaviour
 {
     public Item item;
+    private Image image;
+    private bool IsMouseHovering => RectTransformUtility.RectangleContainsScreenPoint(rectTransform, Input.mousePosition, Camera.main);
+    private RectTransform rectTransform;
+
 
     public bool IsEmpty(){
         //return item == null;
         return transform.childCount == 0;
     }
   
-    public void Dim(){
-
-        
-
+    private void SetAlpha(float alpha){
+        var tempColor = image.color;
+        tempColor.a = alpha;
+        image.color = tempColor;
     }
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
-
+        rectTransform = GetComponent<RectTransform>();
+        image = GetComponent<Image>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(IsMouseHovering){
+            SetAlpha(0.5f);
+        }else{
+            SetAlpha(1f);
+        }
     }
 }
