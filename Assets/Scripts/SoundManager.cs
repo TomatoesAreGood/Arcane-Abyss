63902010dd834666898a43b2096ea0da210132bc
E-Kat101 <katrom567@gmail.com>
2024-01-23 00:52:14 -0500

    Inserted audio clips and created methods to play them

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -7,7 +7,11 @@ public class SoundManager : MonoBehaviour, IDataPersistance
 {
     [SerializeField] Slider VolumeSlider;
     public static SoundManager instance;
-    
+
+
+    [SerializeField] private AudioSource _source;
+    [SerializeField] private AudioClip _playerDamageSFX, _fireBallSFX, _iceSpellSFX, _windSpellSFX, _magicShotSFX, _coinPickUpSFX, _itemPickUpSFX, _enemyDamageSFX;
+
     // Start is called before the first frame update
     private void Start()
     {
@@ -39,4 +43,53 @@ public class SoundManager : MonoBehaviour, IDataPersistance
     {
         data.musicVolume = VolumeSlider.value;
     }
+
+    public void PlayPlayerDamageSFX()
+    {
+        _source.clip = _playerDamageSFX;
+        _source.Play();
+    }
+
+    public void PlayFireBallSFX()
+    {
+        _source.clip = _fireBallSFX;
+        _source.Play();
+    }
+
+    public void PlayIceSpellSFX()
+    {
+        _source.clip = _iceSpellSFX;
+        _source.Play();
+    }
+
+    public void PlayWindSpellSFX()
+    {
+        _source.clip = _windSpellSFX;
+        _source.Play();
+    }
+
+    public void PlayMagicShotSFX()
+    {
+        _source.clip = _magicShotSFX;
+        _source.Play();
+    }
+
+    public void PlayCoinPickUpSFX()
+    {
+        _source.clip = _coinPickUpSFX;
+        _source.Play();
+    }
+
+    public void PlayItemPickUpSFX()
+    {
+        _source.clip = _itemPickUpSFX;
+        _source.Play();
+    }
+
+    public void PlayEnemyDamageSFX()
+    {
+        _source.clip = _enemyDamageSFX;
+        _source.Play();
+    }
+
 }
