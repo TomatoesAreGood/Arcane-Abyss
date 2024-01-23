5d111ccc89392efa8454d670f63321ba7ea3f4d2
E-Kat101 <katrom567@gmail.com>
2024-01-23 13:12:22 -0500

    Added sound effects

--- Assets/Scripts/Spells/IceShotSpell.cs
+++ Assets/Scripts/Spells/IceShotSpell.cs
@@ -10,4 +10,28 @@ public class IceShotSpell : Spell
         manaCost = 15;
         speed = 8;
     }
+    public override void Fire()
+    {
+        if (Time.time >= nextAvailFire && PlayerController.mana - manaCost > 0)
+        {
+            SoundManager.instance.PlayIceSpellSFX();
+
+            Vector3 muzzlePos = PlayerController.instance.firePoint.position;
+            Vector2 shootDirection = PlayerController.instance.firePoint.right;
+
+            float angle = Mathf.Atan2(shootDirection.y, shootDirection.x) * Mathf.Rad2Deg;
+
+            var magicShot = Instantiate(spellShot, muzzlePos, Quaternion.Euler(0f, 0f, angle));
+
+            if (PlayerController.instance.inventory.equippedStaff != null)
+            {
+                magicShot.GetComponent<MagicShot>().AddDamage(PlayerController.instance.inventory.equippedStaff.damageBonus);
+            }
+
+            magicShot.GetComponent<Rigidbody2D>().AddForce(shootDirection * speed, ForceMode2D.Impulse);
+
+            nextAvailFire = Time.time + 1 / fireRate;
+            PlayerController.mana -= manaCost;
+        }
+    }
 }
